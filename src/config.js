e5d49fa64494057a72b578e4c1aae4f784cb55ef
Akash5Chaurasiya <anitachaurasiya9936@gmail.com>
2024-02-23 11:52:10 +0530

    security app

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const RBASE_URL=`https://chawlacomponents.com/api/v1`
+
+
+// export const RBASE_URL=`https://hrms-lix0.onrender.com/api/v1`
+
+// export const RBASE_URL=`https://hrms-backend-04fw.onrender.com/api/v1`
+
+// const RBASE_URL=`https://hrms-backend-04fw.onrender.com/api/v2`
